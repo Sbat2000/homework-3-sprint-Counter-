c2614240c6ab1770f34e3da325765f127b2a681c
Aleksandr Garipov <zevzok@gmail.com>
2022-11-27 20:58:15 +0800

    start and finish homework 3 sprint

--- homework 3 sprint (Counter)/ViewController.swift
+++ homework 3 sprint (Counter)/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labaleCount: UILabel!
+    @IBOutlet weak var buttonAddValueCounter: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    private var counter: Int = 0
+    
+    
+    @IBAction func addValueCounter() {
+        counter+=1
+        labaleCount.text = "Значение счётчика: \(counter)"
+    }
+    
 
 }
 
