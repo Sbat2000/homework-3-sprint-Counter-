26ac930c974bf047303da33fa24907642b0c995f
Aleksandr Garipov <zevzok@gmail.com>
2022-11-27 20:42:35 +0800

    Initial Commit

---
+++ homework 3 sprint (Counter)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework 3 sprint (Counter)
+//
+//  Created by Александр Гарипов on 27.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
